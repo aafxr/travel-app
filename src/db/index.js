b899af863aca0f37a252a4530162cf687ee80fad
Alexandr707 <alexandralennikov90@gmail.com>
2023-06-24 14:52:54 +0700

    поправил коментарии в Database классе

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,17 @@
-import {openDB} from 'idb'
+import {openDB} from 'idb';
+
+/**
+ * @typedef {object} StoreInfo
+ * @property {string} name имя хранилища (store) в бд
+ * @property {string} [key] (optional) (primery key) ключ по которому осуществляется поиск в store
+ * @property {Array.<String>} indexes массив индексов по которым будем искать в store
+ */
 
 /**
  * инициализация базы данных
  * @param {string} dbname                                имя создаваемой базы данных
  * @param {number} version                               версия базы дынных
- * @param {object} stores                                объект содержит name | key | indexes
+ * @param {Array.<StoreInfo>} stores                     массив с инфо о всех хранилищах в бд
  * @returns {Promise<IDBPDatabase<unknown>>}    возвращает Promise с объектом db
  */
 function init({dbname, version, stores}) {
@@ -12,19 +19,17 @@ function init({dbname, version, stores}) {
         upgrade(db) {
             stores.forEach(function (storeInfo) {
                 if (!db.objectStoreNames.contains(storeInfo.name)) {
-                    const store = db.createObjectStore(
-                        storeInfo.name,
-                        {autoIncrement: true}
-                    );
+                    const store = db.createObjectStore(storeInfo.name, {
+                        autoIncrement: true,
+                    });
                     storeInfo.indexes.forEach(function (indexName) {
                         store.createIndex(indexName, indexName, {});
-                    })
+                    });
                 }
-            })
+            });
         },
-    })
+    });
 }
-
 
 export class Database {
     /**
@@ -32,7 +37,7 @@ export class Database {
      *  @constructor
      * @param {string} dbname    имя бд
      * @param {number} version   версия бд
-     * @param {object} stores    объект содержит name | key | indexes
+     * @param {StoreInfo} stores массив с инфо о всех хранилищах в бд
      * @param {function} onReady вызывается если бд откылась без ошибок
      * @param {function} onError вызывается если бд откылась с ошибок
      */
@@ -43,27 +48,27 @@ export class Database {
         init({dbname, version, stores})
             .then((db) => {
                 if (onReady) {
-                    onReady(this)
+                    onReady(this);
                 }
             })
             .catch((err) => {
-                onError(err)
-            })
+                onError(err);
+            });
     }
 
     /**
      * проверяет наличие зранилищя в бд
-     * @param store       объект содержит name | key | indexes
-     * @returns {storeInfo | undefined} storeInfo store | undefined
+     * @param {StoreInfo} store         массив с инфо о всех хранилищах в бд
+     * @returns {StoreInfo | undefined} StoreInfo | undefined
      */
     getStoreInfo(store) {
-        return this.stores.find(item => item.name === store);
+        return this.stores.find((item) => item.name === store);
     }
 
     /**
      * проверяет существует ли index в текущем store
-     * @param indexes           объект содержит name | key | indexes
-     * @param {string} index    index из текущего store
+     * @param {Array.<String> }indexes массив с инфо о всех хранилищах в бд
+     * @param {string} index           index из текущего store
      * @returns {boolean}
      */
     isIndexProp(indexes, index) {
@@ -72,23 +77,22 @@ export class Database {
 
     /**
      * поиск объекта в store по переданным параметрам query
-     * @param store                            объект содержит name | key | indexes
-     * @param query                            параметры поиска
-     * @returns {Promise<any>|Promise<never>}  возвращает Promise с резултатом поиска либо с ошибкой
+     * @param {StoreInfo} store                     массив с инфо о всех хранилищах в бд
+     * @param {String | Number | IDBKeyRange} query параметры поиска
+     * @returns {Promise<any>|Promise<never>}       возвращает Promise с резултатом поиска либо с ошибкой
      */
     getElement(store, query) {
-        const storeInfo = this.getStoreInfo(store)
+        const storeInfo = this.getStoreInfo(store);
         if (storeInfo) {
-            return openDB(this.dbname, this.version)
-                .then(db => {
-                    if (query === 'all')
-                        return db.getAll(store)
-                    if (query instanceof IDBKeyRange)
-                        return db.getAll(store, query)
-                    return db.get(store, query)
-                })
+            return openDB(this.dbname, this.version).then((db) => {
+                if (query === 'all') return db.getAll(store);
+                if (query instanceof IDBKeyRange) return db.getAll(store, query);
+                return db.get(store, query);
+            });
         }
-        return Promise.reject(new Error(`[DB/${this.dbname}]: Store '${store}' not exist`))
+        return Promise.reject(
+            new Error(`[DB/${this.dbname}]: Store '${store}' not exist`)
+        );
     }
 
     /**
@@ -99,21 +103,25 @@ export class Database {
      * @returns {Promise<any>|Promise<never>}      Promise с результатом поиска либо ошибкой
      */
     getFromIndex(store, indexName, query) {
-        const storeInfo = this.getStoreInfo(store)
+        const storeInfo = this.getStoreInfo(store);
         if (storeInfo) {
             if (this.isIndexProp(storeInfo.indexes, indexName)) {
-                return openDB(this.dbname, this.version)
-                    .then(db => {
-                        if (query === 'all')
-                            return db.getAllFromIndex(store, indexName)
-                        if (query instanceof IDBKeyRange)
-                            return db.getAllFromIndex(store, indexName, query)
-                        return db.getFromIndex(store, indexName, query)
-                    })
+                return openDB(this.dbname, this.version).then((db) => {
+                    if (query === 'all') return db.getAllFromIndex(store, indexName);
+                    if (query instanceof IDBKeyRange)
+                        return db.getAllFromIndex(store, indexName, query);
+                    return db.getFromIndex(store, indexName, query);
+                });
             }
-            return Promise.reject(new Error(`[DB/${this.dbname}]: Index ${indexName} not exists in ${store}`))
+            return Promise.reject(
+                new Error(
+                    `[DB/${this.dbname}]: Index ${indexName} not exists in ${store}`
+                )
+            );
         }
-        return Promise.reject(new Error(`[DB/${this.dbname}]: Store '${store}' not exist`))
+        return Promise.reject(
+            new Error(`[DB/${this.dbname}]: Store '${store}' not exist`)
+        );
     }
 
     /**
@@ -123,14 +131,15 @@ export class Database {
      * @returns {Promise<never>|Promise<number | string | Date | ArrayBufferView | ArrayBuffer | IDBValidKey[]>}   Promise с результатом добавления либо ошибкой
      */
     addElement(store, payload) {
-        const storeInfo = this.getStoreInfo(store)
+        const storeInfo = this.getStoreInfo(store);
         if (storeInfo) {
-            return openDB(this.dbname, this.version)
-                .then(db => {
-                    return db.add(store, payload)
-                })
+            return openDB(this.dbname, this.version).then((db) => {
+                return db.add(store, payload);
+            });
         }
-        return Promise.reject(new Error(`[DB/${this.dbname}]: Store '${store}' not exist`))
+        return Promise.reject(
+            new Error(`[DB/${this.dbname}]: Store '${store}' not exist`)
+        );
     }
 
     /**
@@ -140,14 +149,15 @@ export class Database {
      * @returns {Promise<never>|Promise<number | string | Date | ArrayBufferView | ArrayBuffer | IDBValidKey[]>} Promise с результатом обовления либо ошибкой
      */
     editElement(store, payload) {
-        const storeInfo = this.getStoreInfo(store)
+        const storeInfo = this.getStoreInfo(store);
         if (storeInfo) {
-            return openDB(this.dbname, this.version)
-                .then(db => {
-                    return db.put(store, payload)
-                })
+            return openDB(this.dbname, this.version).then((db) => {
+                return db.put(store, payload);
+            });
         }
-        return Promise.reject(new Error(`[DB/${this.dbname}]: Store '${store}' not exist`))
+        return Promise.reject(
+            new Error(`[DB/${this.dbname}]: Store '${store}' not exist`)
+        );
     }
 
     /**
@@ -157,16 +167,15 @@ export class Database {
      * @returns {Promise<never>|Promise<void>}
      */
     removeElement(store, key) {
-        const storeInfo = this.getStoreInfo(store)
+        const storeInfo = this.getStoreInfo(store);
         if (storeInfo) {
-            return openDB(this.dbname, this.version)
-                .then(db => {
-                    if (key === 'all')
-                        return db.clear()
-                    return db.delete(store, key)
-                })
+            return openDB(this.dbname, this.version).then((db) => {
+                if (key === 'all') return db.clear();
+                return db.delete(store, key);
+            });
         }
-        return Promise.reject(new Error(`[DB/${this.dbname}]: Store '${store}' not exist`))
+        return Promise.reject(
+            new Error(`[DB/${this.dbname}]: Store '${store}' not exist`)
+        );
     }
-
 }
