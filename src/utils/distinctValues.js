fdd04dcc671ca6835c68b0b60803ea758f71eb60
OlennikovAV <OlennikovAV@refloor.local>
2023-06-26 14:24:54 +0700

    утилита поиска уникальных строк

---
+++ src/utils/distinctValues.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array.<*>} arr
+ * @callback {(*) => *} transformCb
+ * @returns {unknown[]|*[]}
+ */
+/**
+ * возвращает массив не пересекающихся значений
+ * @param arr массив значений
+ * @param transformCb callback трансформирует массив
+ * @returns {unknown[]|*[]}
+ */
+export default function distinctValues(arr, transformCb){
+    if (Array.isArray(arr)){
+        const set = new Set(arr.map(transformCb))
+        return [...set]
+    }
+    return []
+}
