40903525760d27f8f56312ed6f2febbb5d43cdba
OlennikovAV <OlennikovAV@refloor.local>
2023-06-29 16:59:35 +0700

    утилита модуля Expenses getReportObj

---
+++ src/module/Expenses/utils/getReportObj.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * @param {Array.<import('../models/SectionModel').SectionType>} sections
+ * @param {Array.<import('../models/LimitModel').LimitType>}limits
+ * @param {Array.<import('../models/ExpenseModel').ExpenseType>}expenses
+ * @return {*[]}
+ */
+export default function getReportObj(sections, limits, expenses) {
+    const result = []
+
+    sections.forEach(sec => {
+        let limit = limits.find(l => l.section_id === sec.id) || 0
+        limit && (limit = limit.value)
+        const exp = expenses.filter(e => e.section_id === sec.id)
+        result.push({
+            id: sec.id,
+            name: sec.title,
+            expLimit: limit,
+            expenses: exp
+        })
+    })
+
+    return result
+}
