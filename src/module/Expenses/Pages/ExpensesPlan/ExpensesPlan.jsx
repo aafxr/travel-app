2553f906b2f0fb7baf106263c43b05fa14e0e564
OlennikovAV <OlennikovAV@refloor.local>
2023-06-29 16:50:44 +0700

    страница   план расходов

--- src/module/Expenses/Pages/ExpensesPlan/ExpensesPlan.jsx
+++ src/module/Expenses/Pages/ExpensesPlan/ExpensesPlan.jsx
@@ -2,15 +2,14 @@ import React, {useContext, useEffect, useState} from 'react'
 import {useParams} from "react-router-dom";
 import distinctValues from "../../../../utils/distinctValues";
 
-import ExpensesActionController from '../../controllers/ExpensesActionController'
-import {LocalDB} from "../../../../db";
-import schema from '../../db/schema'
-
 import AddButton from "../../components/AddButtom/AddButton";
 import Line from "../../components/Line/Line";
 import {Input, PageHeader, Tab} from "../../../../components/ui";
 
 import {ExpensesContext} from "../../components/ExpensesContextProvider";
+import getReportObj from "../../utils/getReportObj";
+import Container from "../../components/Container/Container";
+import Section from "../../components/Section/Section";
 
 
 export default function ExpensesPlan({
@@ -20,39 +19,63 @@ export default function ExpensesPlan({
     const {travelCode: primary_entity_id} = useParams()
     const {controller} = useContext(ExpensesContext)
 
+    const [limits, setLimits] = useState([])
     const [sections, setSections] = useState([])
     const [expenses, setExpenses] = useState([])
 
-        useEffect(() => {
+
+    //получаем все лимиты на поездку
+    useEffect(() => {
         if (controller) {
-            controller.sectionModel.get('all')
-                .then(items => distinctValues(items, item => item.id))
-                .then(items => setSections(items))
+            controller.limitModel.getFromIndex('primary_entity_id', IDBKeyRange.only(primary_entity_id))
+                .then(items => setLimits(items))
                 .catch(console.error)
         }
-    }, [])
+    }, [controller])
 
+
+    //получаем все секции для лимитов поездки
     useEffect(() => {
-        if (sections.length) {
-            controller.expensesActualModel.getFromIndex('section_id', IDBKeyRange.only(sections[0]))
+        async function getSections() {
+            if (limits.length) {
+                const res = []
+                for (const limit of limits) {
+                    const sec = await controller.sectionModel.get(limit.section_id)
+                    sec && res.push(sec)
+                }
+                setSections(res)
+            }
+        }
+
+        getSections().catch(console.error)
+
+    }, [limits])
+
+
+    //получаем все расходы за поездку
+    useEffect(() => {
+        if (sections && sections.length) {
+            controller.expensesPlanedModel.getFromIndex('primary_entity_id', IDBKeyRange.only(primary_entity_id))
                 .then(setExpenses)
                 .catch(console.error)
         }
     }, [sections])
 
-    console.log(expenses)
+
+    const report = sections.length && limits.length && expenses.length && getReportObj(sections, limits, expenses) || []
+
 
     return (
         <>
-            <div style={{padding: '0 20px'}}>
-                <AddButton>Записать расходы</AddButton>
-                <Line value={0.3} color={'#52CF37'}/>
-                <br/>
-                <Line value={0.8} color={'red'}/>
-                <br/>
-                <Input placeholder={'Название'}/>
-                <br/>
-                <Input placeholder={'Сумма '}/>
+            <div >
+                <AddButton to={`/travel/${primary_entity_id}/expenses/plan/add/`}>Записать расходы</AddButton>
+                <Container>
+                    {
+                        !!report.length && report.map(item => (
+                            <Section key={item.id} {...item} />
+                        ))
+                    }
+                </Container>
             </div>
         </>
     )
