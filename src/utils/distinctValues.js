05a1a9567b1ea02b80fcf07a5145db57d36436ab
OlennikovAV <OlennikovAV@refloor.local>
2023-07-05 10:08:40 +0700

    distinctValues

--- src/utils/distinctValues.js
+++ src/utils/distinctValues.js
@@ -1,18 +1,15 @@
 /**
- * @param {Array.<*>} arr
- * @callback {(*) => *} transformCb
- * @returns {unknown[]|*[]}
- */
-/**
  * возвращает массив не пересекающихся значений
  * @param arr массив значений
  * @param transformCb callback трансформирует массив
  * @returns {unknown[]|*[]}
  */
-export default function distinctValues(arr, transformCb){
-    if (Array.isArray(arr)){
-        const set = new Set(arr.map(transformCb))
-        return [...set].map(i => i)
+export default function distinctValues(arr, transformCb) {
+    if (Array.isArray(arr)) {
+        const set = new Set(arr.filter(item => item || typeof item === 'number').map(transformCb))
+        return [...set]
+    } else if (arr) {
+        return [transformCb(arr)]
     }
     return []
 }
