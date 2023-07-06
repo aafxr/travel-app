ec37c6ccf716b3084cea2296cbe8146e1eaa5645
OlennikovAV <OlennikovAV@refloor.local>
2023-07-06 10:50:15 +0700

    distinctValues добавил возможность добавлять спиок дефолтных значений

--- src/utils/distinctValues.js
+++ src/utils/distinctValues.js
@@ -1,12 +1,18 @@
 /**
  * возвращает массив не пересекающихся значений
- * @param arr массив значений
+ * @param {Array} arr массив значений
  * @param transformCb callback трансформирует массив
+ * @param {Array.<string>} [extraValues] трансформирует массив
  * @returns {unknown[]|*[]}
  */
-export default function distinctValues(arr, transformCb) {
+export default function distinctValues(arr, transformCb, extraValues) {
     if (Array.isArray(arr)) {
         const set = new Set(arr.filter(item => item || typeof item === 'number').map(transformCb))
+        if (extraValues && Array.isArray(extraValues)) {
+            extraValues
+                .filter(e => !!e)
+                .forEach(e => set.add(e))
+        }
         return [...set]
     } else if (arr) {
         return [transformCb(arr)]
