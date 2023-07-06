0ebabac8a2eaabd586a04427309a7e3895d9254b
OlennikovAV <OlennikovAV@refloor.local>
2023-07-06 16:11:24 +0700

    Chip css -> flex

--- src/components/ui/Chip/Chip.jsx
+++ src/components/ui/Chip/Chip.jsx
@@ -1,45 +1,51 @@
 import React from 'react'
 import clsx from 'clsx'
-import st  from './Chip.module.css'
 import isString from "../../../utils/validation/isString";
+
+import  './Chip.css'
 
 
 /**
  * компонент для отображения тегов / пометок
- * @param {'orange' | 'green' | 'gray' | 'light-orange'} color
+ * @param {'orange' | 'green' | 'gray' | 'light-orange' } color
  * @param {string} icon - url иконки
  * @param {'left' | 'right'} iconPosition - способ расположения иконки (стиль применяется если задан icon) default = 'left'
  * @param {boolean} rounded - способ скругления краев, default - более прямоугольная форма
+ * @param {boolean} pointer default = false
  * @param {JSX.Element | string | number} children
  * @param {string} className
  * @param props
  * @returns {JSX.Element}
  */
 export default ({
-    color = 'orange', // 'orange' | 'green' | 'gray' | 'light-orange'
-    icon,
-    iconPosition = 'left',// 'left' | 'right'
-    children,
-    rounded,// boolean
-    className,
-    ...props
+                    color = 'orange', // 'orange' | 'green' | 'gray' | 'light-orange'
+                    icon,
+                    iconPosition = 'left',// 'left' | 'right'
+                    children,
+                    rounded,// boolean
+                    pointer = false,
+                    className,
+                    ...props
 
-})=>{
-    const classes = clsx({
-        [st.chip]: true,
-        [st.icon]: icon,
-        [st.orange]: color === 'orange',
-        [st.green]: color === 'green',
-        [st['gray']]: color === 'grey',
-        [st['light-orange']]: color === 'light-orange',
-        [st['icon-left']]: icon && iconPosition === 'left',
-        [st['icon-right']]: icon && iconPosition === 'right',
-        [st.rounded]: rounded,
+                }) => {
+    const classes = clsx(
+        {
+            ['chip']: true,
+            ['chip-icon']: icon,
+            ['chip-pointer']: pointer,
+            ['chip-orange']: color === 'orange',
+            ['chip-green']: color === 'green',
+            ['chip-gray']: color === 'grey',
+            ['chip-light-orange']: color === 'light-orange',
+            ['chip-icon-left']: icon && iconPosition === 'left',
+            ['chip-icon-right']: icon && iconPosition === 'right',
+            ['chip-rounded']: rounded,
+        },
         className
-    })
+    )
 
     return <div className={classes} {...props}>
-        {isString(icon) && <img src={icon} alt="icon"/> }
-        {children}
+        {isString(icon) && <img src={icon} alt="icon"/>}
+        <span>{children}</span>
     </div>
 }
