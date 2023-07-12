f815e9bcafb6fa60d7841c9c357209f11cc4acae
OlennikovAV <OlennikovAV@refloor.local>
2023-07-12 16:59:47 +0700

    hook useToBottomHeight

---
+++ src/modules/Expenses/hooks/useToBottomHeight.js
@@ -0,0 +1,15 @@
+import {useEffect, useRef} from "react";
+
+export default function useToBottomHeight(bottomMargin = 0){
+    const ref = useRef()
+
+
+    useEffect(()=>{
+        if (ref && ref.current){
+            //Element Height = Viewport height - element.offset.top - desired bottom margin
+            ref.current.style.height = window.innerHeight - ref.current.offsetTop - bottomMargin + 'px'
+        }
+    },[ref.current])
+
+    return ref
+}
