8c7530e97f149341ba79baa0265f032da2a11eb0
OlennikovAV <OlennikovAV@refloor.local>
2023-07-18 17:01:47 +0700

    Select component

--- src/components/ui/Select/Select.jsx
+++ src/components/ui/Select/Select.jsx
@@ -1,40 +1,59 @@
 import React, {useState} from 'react'
 import clsx from "clsx";
 
+import './Select.css'
+import useOutside from "../../../hooks/useOutside";
 
- function Select({options, className, size = 4, ...props}, ref){
-     const [selected, setSelected] = useState((options && options[0]) || '')
 
-     function onSelectHandler(value){
-         setSelected(value)
-     }
+function Select({defaultValue = '', options, className, size = 4, border, ...props}, ref) {
+    const [selected, setSelected] = useState(defaultValue)
+    const [active, setActive] = useState(false)
+    const {ref: selectMainRef} = useOutside(active, setActive.bind(this, () => false))
+
+    function onSelectHandler(value) {
+        if (active) {
+            setSelected(value)
+            setActive(!active)
+        }
+    }
+
+    const selectStyle = {
+        maxHeight: `calc( (var(--select-padding) * 2 + var(--select-height)) * ${active ? size + 1 : 1})`,
+    }
+
+    const selectClasses = clsx(
+        'select column',
+        {
+            'active': active,
+            'border': border
+        },
+        className
+    )
+
 
     return (
-        <div className={clsx('select', className)}
-        style={{
-            height: `calc(var(--select-height) * ${size})`,
-        }}
+        <div ref={selectMainRef} className={selectClasses}
+             style={selectStyle}
         >
-            <div className='select-value'>
-                {selected || ''}
+            <div className='select-header flex-between' onClick={() => setActive(!active)}>
+                <div className='select-value center'>
+                    {selected || ''}
+                </div>
+                <div className='select-chevron'/>
             </div>
-            <select ref={ref} {...props} hidden>
+            <select ref={ref} value={selected} onChange={() => {
+            }} {...props} hidden>
                 {
-                    options && options.length && options.map( o => (
-                        <option key={o} value={o} selected={selected === o}>{o}</option>
+                    options && options.length && options.map(o => (
+                        <option key={o} value={o}>{o}</option>
                     ))
                 }
             </select>
 
             {
-                options && options.length && options.map( o => (
-                    <div  className={clsx(
-                        'select-item',
-                        {
-                            'selected': selected === o,
-                        }
-                    )}
-                          onClick={()=> onSelectHandler(o)}
+                options && options.length && options.map(o => (
+                    <div key={o} className={clsx('select-item center', {'selected': selected === o})}
+                         onClick={() => onSelectHandler(o)}
                     >
                         {o}
                     </div>
