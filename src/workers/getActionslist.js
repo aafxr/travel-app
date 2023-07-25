a32ef6a910835e8a31dcddfd0cb5cef31aef60fb
OlennikovAV <OlennikovAV@refloor.local>
2023-07-25 08:38:07 +0700

    25/07/2023

--- src/workers/getActionslist.js
+++ src/workers/getActionslist.js
@@ -3,6 +3,7 @@ import Model from "../models/Model";
 import schema from "../modules/Expenses/db/schema";
 import constants from "../modules/Expenses/db/constants";
 import actionsValidation from "../modules/Expenses/models/action/validation";
+import functionDurationTest from "../utils/functionDurationTest";
 
 
 let ready = false
@@ -22,16 +23,20 @@ export default async function getActionsList() {
     if (ready) {
         const response = await fetch(process.env.REACT_APP_SERVER_URL + '/expenses/getActions/')
         const receivedActions = await response.json()
-        console.log('Received data: ',receivedActions)
+        console.log('Received data: ', receivedActions)
+
 
         if (receivedActions.ok && receivedActions.result) {
-            const actions = await actionsModel.getFromIndex(constants.indexes.SYNCED, 1)
-            const existingActions = actions.reduce((acc, a) => {
-                acc[a.uid] = a
-                return acc
-            }, {})
-            const filtered = receivedActions.result.filter(a => !existingActions[a.uid])
-            postMessage(filtered)
+            functionDurationTest(async () => {
+                const actions = await actionsModel.getFromIndex(constants.indexes.SYNCED, 1)
+                const existingActions = actions.reduce((acc, a) => {
+                    acc[a.uid] = a
+                    return acc
+                }, {})
+                const filtered = receivedActions.result.filter(a => !existingActions[a.uid])
+                postMessage(filtered)
+            }, '[Worker] без fetch: ')
         }
+
     }
 }
