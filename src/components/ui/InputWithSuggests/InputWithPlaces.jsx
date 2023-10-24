f4e83240364a8f153d783ce0932514ebb889d169
aafxr <alennikov@refloor-nsk.ru>
2023-10-24 09:02:41 +0700

    debounce request for places

--- src/components/ui/InputWithSuggests/InputWithPlaces.jsx
+++ src/components/ui/InputWithSuggests/InputWithPlaces.jsx
@@ -1,26 +1,41 @@
-import {Input} from "../index";
+import {forwardRef} from 'react'
+
+import debounce from "lodash.debounce";
 import aFetch from "../../../axios";
+import {Input} from "../index";
 
-export default function InputWithPlaces({onPlaces, ...props}) {
+/**
+ * компонент делает запрос на получение рекомендуемых мест для посещения и в случае успеха возвращает результат путем
+ * вызова колбэка "onPlaces"
+ * @function
+ * @name InputWithPlaces
+ * @param {function} onPlaces callback, вызывается в случае успешного получения рекомендуемых мест для посещения
+ * @param {number} delay время (в миллисекундах) задержки запроса на получение рекомендуемых мест для посещения
+ * @param props other props
+ * @param {React.Ref} ref react ref
+ * @returns {JSX.Element}
+ */
+function InputWithPlaces({onPlaces, delay = 300, ...props}, ref) {
 
-    /**
-     *
+
+    /***
      * @param {InputEvent} e
      */
-    function handleInputChange(e) {
+    const handleInputChange = debounce((e) =>  {
         aFetch.post('/places/getList/', {text: e.target.value})
             .then(resp => resp.data)
             .then(resp => {
                 console.log(resp)
                 if ("ok" in resp && onPlaces)
                     onPlaces(Array.isArray(resp.data) ? resp.data : [])
-
             })
             .catch(console.error)
         props.onChange && props.onChange(e)
-    }
+    }, 500, {trailing:true})
 
     return (
-        <Input {...props} onChange={handleInputChange}/>
+        <Input ref={ref} {...props} onChange={handleInputChange}/>
     )
 }
+
+export default forwardRef(InputWithPlaces)
