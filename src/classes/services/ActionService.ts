c24af310d8388d7749ec90a64fe29a54b8404151
aafxr <alexandral.fed23@gmail.com>
2024-03-05 15:15:03 +0700

    action service description

--- src/classes/services/ActionService.ts
+++ src/classes/services/ActionService.ts
@@ -3,7 +3,13 @@ import {IndexName} from "../../types/IndexName";
 import {Action} from "../StoreEntities";
 import {DB} from "../db/DB";
 
+/**
+ * сервис для рабботы с actions
+ */
 export class ActionService{
+    /**
+     * методпозволяет полусить время последнего action хранимого в локальной бд
+     */
     static async getLastActionTime(){
         const cursor = await DB.openIndexCursor<Action<any>>(StoreName.ACTION, IndexName.DATETIME, IDBKeyRange.upperBound(new Date()), "prev")
         const action = (await cursor.next()).value
