ad9dee414e98a0a8f9f2a925eb581a13f0ad2a65
aafxr <alexandral.fed23@gmail.com>
2024-04-08 11:32:49 +0700

    modify action date fields from iso string to Date inctanse

--- src/api/fetch/fetchActions.ts
+++ src/api/fetch/fetchActions.ts
@@ -7,10 +7,22 @@ type ResponseType<T extends {}> = {
     message?: string
 }
 
+const dateKeys = ['created_at', 'updated_at']
+
 export async function fetchActions<T extends {}>(time_ms:number){
     const result = (await aFetch.post<ResponseType<T>>('/actions/get/', {time: time_ms})).data
     if(result.ok && result.data){
-        return result.data.map(a => new Action<T>(a.data, a.user_id, a.entity, a.action))
+        const actions = result.data.map(a => new Action<T>(a.data, a.user_id, a.entity, a.action))
+        actions.forEach(a => {
+            Object.keys(a.data)
+                .forEach(key => {
+                    if(key.includes('date') || dateKeys.includes(key)){
+                        // @ts-ignore
+                        a.data[key] = new Date(a.data[key])
+                    }
+                })
+        })
+        return actions
     } else {
         return []
     }
